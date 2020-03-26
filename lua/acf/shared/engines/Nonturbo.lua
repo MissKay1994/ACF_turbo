4e16d12a0f1b8872164093cafce4bf97afb1c12d
MissKay1994 <cab1994@hotmail.com>
2020-03-26 19:55:56 -0400

    Update Nonturbo.lua

--- lua/acf/shared/engines/Nonturbo.lua
+++ lua/acf/shared/engines/Nonturbo.lua
@@ -6,7 +6,7 @@ ACF_DefineEngine( "1.8L-B4", {
 	name = "1.8L Flat 4 Petrol",
 	desc = "Small economic flat 4 found in sedans",
 	model = "models/engines/b4med.mdl",
-	sound = "acf_engines/b4_petrolmedium.wav",
+	sound = "acf_engines/b4_petrolsmall.wav",
 	category = "Nonturbo",
 	fuel = "Petrol",
 	enginetype = "GenericPetrol",
