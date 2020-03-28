bb62667e43b72aafdf1299662e44686d76f07b6a
MissKay1994 <cab1994@hotmail.com>
2020-03-27 21:57:52 -0400

    Fixes
    Fixed the HP of Flat 4's
    Added 2.7L Flat 6

---
+++ lua/acf/shared/engines/NewB6.lua
@@ -0,0 +1,21 @@
+
+-- Flat 6 engines
+
+ACF_DefineEngine( "2.7-B6", {
+	name = "2.7L Flat 6 Petrol",
+	desc = "Flat 6 from a German sports car",
+	model = "models/engines/b6small.mdl",
+	sound = "acf_engines/b6_petrolsmall.wav",
+	category = "NewB6",
+	fuel = "Petrol",
+	enginetype = "GenericPetrol",
+	weight = 180,
+	torque = 212,
+	flywheelmass = 0.08,
+	idlerpm = 750,
+	peakminrpm = 4300,
+	peakmaxrpm = 6950,
+	limitrpm = 7250
+} )
+
+
